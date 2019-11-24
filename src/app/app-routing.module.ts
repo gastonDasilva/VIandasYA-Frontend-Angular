ab209db95e9ec53f45075b51090d6bab2a034d32
Gaston Da SIlva <gastonCss_16@hotmail.com>
2019-11-24 18:43:12 -0300

    se agrego el inicio de sesion con google

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { InterceptorService } from './interceptor.service';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,50 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { InterceptorService } from './interceptor.service';
+import { HomeComponent } from './home/home.component';
+
+// Importacion de clases para el inicio de session con las redes sociales
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
+import { LoginComponent } from './login/login.component';
+
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("295956537744-vo03s8f02mtn42nv37g2vubjqmlfrqi5.apps.googleusercontent.com")
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider("Facebook-App-Id")
+  }
+]);
+
+ export function provideConfig() {
+   return config;
+ }
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    SocialLoginModule
   ],
   providers: [{
                     provide: HTTP_INTERCEPTORS,
                     useClass: InterceptorService,
                     multi: true
-                  }
+                  },
+   {
+     provide: AuthServiceConfig,
+     useFactory: provideConfig
+   }
 ],
   bootstrap: [AppComponent]
 })
